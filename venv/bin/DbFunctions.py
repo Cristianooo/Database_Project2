f406efc5d258598135bf4d5e14fe9e6e148be2b9
Cristiano Firmani <cristianof@icloud.com>
2019-02-26 21:50:32 -0800

    Added main, made it pretty

--- venv/bin/DbFunctions.py
+++ venv/bin/DbFunctions.py
@@ -12,7 +12,7 @@ class DbFunctions:
         for row in rows:
             print(row)
             print('')
-        time.sleep(2)
+        time.sleep(1)
 
     def createStudent(self):
         inputCheck = True
@@ -31,8 +31,13 @@ class DbFunctions:
         c.execute("INSERT INTO Students (FirstName,LastName,GPA,Major,FacultyAdvisor) VALUES (?, ?, ?, ?, ?)",
                   (tempStudent.getStudentTuple()))
         conn.commit()
+        print("Student Added!")
+        time.sleep(1)
+
     def updateStudent(self):
-        sidEdit = int(input("Enter the desired StudentID you want to Edit: "))
+        print("This is the current table: ")
+        DbFunctions.displayStudents(self)
+        sidEdit = int(input("Enter the desired StudentID you want to edit: "))
         if(DbFunctions.checkIfStudentExists(self, sidEdit)== True):
             newMajor = raw_input("Please input the updated Major for this Student: ")
             newAdvisor = raw_input("Please input the updated Advisor for this Student: ")
@@ -41,7 +46,7 @@ class DbFunctions:
             conn.commit()
         else:
             print("Invalid student ID. ")
-            time.sleep(2)
+            time.sleep(1)
 
     def checkIfStudentExists(self, SID):
         c.execute("SELECT StudentId FROM Students WHERE StudentId = ?", (SID,))
@@ -51,10 +56,100 @@ class DbFunctions:
         return True;
 
     def deleteStudent(self):
+        print("This is the current table: ")
+        DbFunctions.displayStudents(self)
         sidDelete = int(input("Enter the desired StudentID you want to Remove: "))
         if (DbFunctions.checkIfStudentExists(self, sidDelete) == True):
             c.execute("DELETE FROM Students WHERE StudentId = ?", (sidDelete,))
             conn.commit()
+            print("Student Deleted.")
+            time.sleep(1)
         else:
             print("Invalid student ID. ")
-            time.sleep(2)
+            time.sleep(1)
+
+
+    def searchStudents(self):
+        print("Enter what you would like to display the students by: ")
+        inputCheck = True
+        while (inputCheck):
+            try:
+                sidDisplay = int(input("1) By Major  2) By GPA  3) By Advisor: "))
+                inputCheck = False
+            except NameError:
+                print("Enter a proper number (1, 2, or 3)")
+                print('')
+                time.sleep(0.5)
+        if (sidDisplay == 1):
+            majorSelection = raw_input("What major would you like to sort by: ")
+            c.execute("SELECT * FROM Students WHERE Major = ?", (majorSelection,))
+            rows = c.fetchall()
+            if (rows == []):
+                print("No students Found")
+                time.sleep(0.5)
+            else:
+                for row in rows:
+                    print(row)
+                time.sleep(0.5)
+        elif (sidDisplay == 2):
+            inputCheck = True
+            while(inputCheck):
+                try:
+                    GPASelection = int(input("Do you want to sort by 1) an upper bound or 2) a lower bound? "))
+                    inputCheck = False
+                except NameError:
+                    print("Enter a proper number (1 or 2)")
+                    print('')
+                    time.sleep(0.5)
+            if (GPASelection == 1):
+                inputCheck = True
+                while (inputCheck):
+                    try:
+                        upperBound = float(input("What is your upper bound? "))
+                        inputCheck = False
+                    except NameError:
+                        print("Enter a proper GPA")
+                        print('')
+                        time.sleep(0.5)
+                c.execute("SELECT * FROM Students WHERE GPA < ? ", (upperBound,))
+                rows = c.fetchall()
+                if (rows == []):
+                    print("No Students found")
+                    time.sleep(0.5)
+                else:
+                    for row in rows:
+                        print(row)
+                    time.sleep(0.5)
+            elif (GPASelection == 2):
+                inputCheck = True
+                while (inputCheck):
+                    try:
+                        lowerBound = float(input("What is your lower bound? "))
+                        inputCheck = False
+                    except NameError:
+                        print("Enter a proper GPA")
+                        print('')
+                        time.sleep(0.5)
+                c.execute("SELECT * FROM Students WHERE GPA > ? ", (lowerBound,))
+                rows = c.fetchall()
+                if(rows==[]):
+                    print("No Students found")
+                    time.sleep(0.5)
+                else:
+                    for row in rows:
+                        print(row)
+                    time.sleep(0.5)
+        elif (sidDisplay == 3):
+            advisorSelection = raw_input("What is the advisor you want to sort by: ")
+            c.execute("SELECT * FROM Students WHERE FacultyAdvisor = ? ", (advisorSelection,))
+            rows = c.fetchall()
+            if (rows == []):
+                print("No Students found")
+                time.sleep(0.5)
+            else:
+                for row in rows:
+                    print(row)
+                time.sleep(0.5)
+        else:
+            print("Invalid number inputted.")
+            time.sleep(1)
--- venv/bin/Driver.py
+++ venv/bin/Driver.py
@@ -2,72 +2,51 @@ import sqlite3
 
 from Student import Student
 from DbFunctions import DbFunctions
+
 conn = sqlite3.connect('Students')
 c = conn.cursor()
 c.execute("CREATE TABLE IF NOT EXISTS Students(StudentId INTEGER PRIMARY KEY AUTOINCREMENT,FirstName VARCHAR(25) NOT NULL,LastName VARCHAR(25) NOT NULL,GPA NUMERIC NOT NULL ,Major VARCHAR(10) NOT NULL,FacultyAdvisor VARCHAR(25) NOT NULL)")
-
-myFunctions = DbFunctions()
-keepLoop = True
-while(keepLoop):
-
-    print(
-        '''
-        1) Display ALL Students
-        2) Create/Add a Student
-        3) Update Student Info
-        4) Delete Student by SID
-        5) Search for Student (GPA, Major, Advisor)
-        6+) Exit
-        '''
-    )
-
-    print("What would you like to do?")
-    userChoice = int(input("# "))
-
-    if(userChoice == 1):
-        myFunctions.displayStudents()
-
-    elif(userChoice == 2):
-        myFunctions.createStudent()
+class Driver:
 
 
-    elif(userChoice == 3):
-        myFunctions.updateStudent()
 
-    elif(userChoice == 4):
-        myFunctions.deleteStudent()
-    elif (userChoice == 5):
-        print("Enter what you would like to display the students by: ")
-        sidDisplay = int(input("1) By Major  2) By GPA  3) By Advisor: "))
-        if (sidDisplay == 1):
-            majorSelection = raw_input("What major would you like to sort by: ")
-            c.execute("SELECT * FROM Students WHERE Major = ?", (majorSelection,))
-            rows = c.fetchall()
-            for row in rows:
-                print(row)
-        elif (sidDisplay == 2):
-            GPASelection = int(input("Do you want to sort by 1) an upper bound or 2) a lower bound? "))
-            if (GPASelection == 1):
-                upperBound = float(input("What is your upper bound? "))
-                c.execute("SELECT * FROM Students WHERE GPA < ? ", (upperBound,))
-                rows = c.fetchall()
-                for row in rows:
-                    print(row)
-            elif(GPASelection == 2):
-                lowerBound = float(input("What is your lower bound? "))
-                c.execute("SELECT * FROM Students WHERE GPA > ? ", (lowerBound,))
-                rows = c.fetchall()
-                for row in rows:
-                    print(row)
-        elif(sidDisplay == 3):
-            advisorSelection = raw_input("What is the advisor you want to sort by: ")
-            c.execute("SELECT * FROM Students WHERE FacultyAdvisor = ? ", (advisorSelection,))
-            rows = c.fetchall()
-            for row in rows:
-                print(row)
-    else:
-        keepLoop = False
-        c.close()
+
+    def run(self):
+        myFunctions = DbFunctions()
+        keepLoop = True
+        while(keepLoop):
+
+            print(
+                '''
+                1) Display ALL Students
+                2) Create/Add a Student
+                3) Update Student Info
+                4) Delete Student by SID
+                5) Search for Student (Major, GPA, Advisor)
+                6+) Exit
+                '''
+            )
+
+            print("What would you like to do?")
+            userChoice = int(input("# "))
+
+            if(userChoice == 1):
+                myFunctions.displayStudents()
+
+            elif(userChoice == 2):
+                myFunctions.createStudent()
+
+
+            elif(userChoice == 3):
+                myFunctions.updateStudent()
+
+            elif(userChoice == 4):
+                myFunctions.deleteStudent()
+            elif (userChoice == 5):
+                myFunctions.searchStudents()
+            else:
+                keepLoop = False
+                c.close()
 
 
 
